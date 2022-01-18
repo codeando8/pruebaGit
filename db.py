2716ab24b48212728e3d74594ea12f396eea58be
pepitoname <pepito>
2022-01-18 11:40:10 -0500

    Este es el msg del 1er commit

---
+++ db.py
@@ -0,0 +1,37 @@
+import pymysql
+
+class DataBase:
+   
+
+    def seleccionarCantidad(self, codigo):
+        sql = 'SELECT cantidad FROM Datos_articulo WHERE Codigo_articulo={}'.format(codigo)
+        cantidad = -1
+        try:
+            self.cursor.execute(sql)
+            item = self.cursor.fetchone()
+            cantidad = item[0]
+        except Exception as e:
+            raise 
+        return cantidad
+    
+    def actualizarCantidad(self, codigo, cantidad):
+        sql = 'UPDATE Datos_articulo SET cantidad={} where Codigo_articulo={}'.format(cantidad,codigo)
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except Exception as e:
+            raise 
+    
+    def nombreProducto(self, codigo):
+        sql = 'SELECT nombre FROM Datos_articulo WHERE Codigo_articulo={}'.format(codigo)
+        nombre = ''
+        try:
+            self.cursor.execute(sql)
+            item = self.cursor.fetchone()
+            nombre = item[0]
+        except Exception as e:
+            raise 
+        return nombre
+
+    def close(self):
+        self.connection.close()
